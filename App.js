a97f2912716d1acbe70d43f1689e837b29d14e11
Tsoomoo94 <Gtsolmontuya@gmail.com>
2022-01-13 21:26:37 +0800

    zaswar

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
-
+import React from 'react'
+import {Text} from 'react-native';
 import codePush from 'react-native-code-push';
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  View,
-} from 'react-native';
+
 
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View style={styles.body}>
-            <Text>Test</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+       <Text>aaa</Text>
     </>
   );
 };
 
 export default codePush(App);
-
